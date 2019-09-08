d26fc7bf64809ee2649bb18ebac73a7625334bd8
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2019-09-08 18:32:04 -0300

    Add files via upload

---
+++ FDS/index.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector("#meubotaozinho2");
+
+let body = document.querySelector("#f")
+
+botao.addEventListener('click', function() {
+body.style.backgroundColor = "yellow"
+})
