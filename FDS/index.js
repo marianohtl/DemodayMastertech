daf7d2957a51ad9c5744493d33034fb1788bd322
Thalita Mariano <37271065+marianohtl@users.noreply.github.com>
2020-01-22 00:31:54 -0300

    organizando rep. em branches

--- FDS/index.js
+++
@@ -1,7 +0,0 @@
-let botao = document.querySelector("#meubotaozinho2");
-
-let body = document.querySelector("#f")
-
-botao.addEventListener('click', function() {
-body.style.backgroundColor = "yellow"
-})
